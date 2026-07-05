ecffe5d887b25b10a16be54693d9a3ee90d9f2f7
Christian Akabueze <130872924+fechachris4@users.noreply.github.com>
2026-07-06 00:23:39 +0100

    Move FK validation plot into plotting/ module, slim main.py
    - plotting/fk_validation.py: FKValidationPlot class + standalone entry
      point (python -m plotting.fk_validation), saves PNG to plots/ on exit
    - main.py: back to bare sim loop; drop broken 'from pd import
      controller_step' (controller/pd.py is empty, no controller_step exists)

--- main.py
+++ main.py
@@ -1,61 +1,14 @@
 import time
-from collections import deque
 
-import matplotlib.pyplot as plt
 import mujoco
 import mujoco.viewer
 
 from sim import world
-from pd import controller_step
-from controller import kinematics
-
-BUFFER_SIZE = 2000
-PLOT_EVERY_N_STEPS = 25
-
-plt.ion()
-fig, ax = plt.subplots()
-ax.set_xlabel("sim time (s)")
-ax.set_ylabel("position (m)")
-
-times = deque(maxlen=BUFFER_SIZE)
-series = {
-    "MuJoCo X": (deque(maxlen=BUFFER_SIZE), "tab:blue"),
-    "FK X": (deque(maxlen=BUFFER_SIZE), "tab:orange"),
-    "MuJoCo Y": (deque(maxlen=BUFFER_SIZE), "tab:green"),
-    "FK Y": (deque(maxlen=BUFFER_SIZE), "tab:red"),
-    "MuJoCo Z": (deque(maxlen=BUFFER_SIZE), "tab:purple"),
-    "FK Z": (deque(maxlen=BUFFER_SIZE), "tab:brown"),
-}
-lines = {
-    name: ax.plot([], [], color=color, label=name)[0]
-    for name, (_, color) in series.items()
-}
-ax.legend(loc="upper right")
 
 with mujoco.viewer.launch_passive(world.model, world.data) as viewer:
-    step_count = 0
     while viewer.is_running():
         step_start = time.time()
         mujoco.mj_step(world.model, world.data)
-
-        mujoco_pos = world.data.site_xpos[world.right_ee_id]
-        fk_pos, _ = kinematics.right_ee_positions()
-
-        times.append(world.data.time)
-        for axis, name_pair in enumerate(
-            [("MuJoCo X", "FK X"), ("MuJoCo Y", "FK Y"), ("MuJoCo Z", "FK Z")]
-        ):
-            series[name_pair[0]][0].append(mujoco_pos[axis])
-            series[name_pair[1]][0].append(fk_pos[axis])
-
-        step_count += 1
-        if step_count % PLOT_EVERY_N_STEPS == 0:
-            for name, (buffer, _) in series.items():
-                lines[name].set_data(times, buffer)
-            ax.relim()
-            ax.autoscale_view()
-            fig.canvas.draw_idle()
-            fig.canvas.flush_events()
 
         viewer.sync()
 
